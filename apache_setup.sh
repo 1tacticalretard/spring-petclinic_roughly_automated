76710a039221e4e0d8c3329ae451a3cf37d0e480
1tacticalretard <youremail@domain.com>
2021-09-27 13:42:06 +0300

    1st commit

--- apache_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt -y update
-sudo apt -y install apache2
-MyIP=`curl -4 icanhazip.com`
-echo "Apache WS is active. Server IP: $MyIP" > /var/www/html/index.html
-sudo systemctl start apache2.service
